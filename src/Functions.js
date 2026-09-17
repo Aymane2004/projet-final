b0663d9d75bb3b4f2aaaaf52b2e06a847b730c25
Aymane2004 <aymantt2004@gmail.com>
2026-09-17 17:44:42 +0200

    added functions to the program

--- src/Functions.js
+++ src/Functions.js
@@ -1,13 +1,13 @@
 import apprenants from './data.js'
 
 
-export function normaliserNom(nom){
-    let x = nom.trim().replaceAll(',','').replace(/\s+/g, ' ');
+export function normaliserNom(nom) {
+    let x = nom.trim().replaceAll(',', '').replace(/\s+/g, ' ');
     return x
 }
 console.log(normaliserNom('   ayman    taleb  '))
 
-export function validerResultat(jour, propose, termine, challenge){
+export function validerResultat(jour, propose, termine, challenge) {
     if (isNaN(jour) || jour > 7 || jour < 1) {
         return 'Veuillez saisir un jour entre 1 et 7';
     }
@@ -31,37 +31,39 @@ export function validerResultat(jour, propose, termine, challenge){
     return true;
 
 }
-console.log(validerResultat(5,20,21,true))
+console.log(validerResultat(5, 20, 21, true))
 
-export function ajouterApprenant(nom, ville){
+export function ajouterApprenant(nom, ville) {
     let id = apprenants.length + 1;
     let nomC = normaliserNom(nom)
     apprenants.push({
-        id:id,
-        nomComplet:nomC,
-        ville:ville,
-        resultats:[]})
+        id: id,
+        nomComplet: nomC,
+        ville: ville,
+        resultats: []
+    })
     return apprenants
 }
-console.log(ajouterApprenant('ahmed','Casablanca'))
+console.log(ajouterApprenant('ahmed', 'Casablanca'))
 
-export function enregistrerResultat(id, jour, propose, termine, challenge){
-    let a = apprenants.find(i=>i.id == id)
-    if(a){
-        if(validerResultat(jour,propose,termine,challenge)){
-            a.resultats.push({jour:jour,totalExercices:propose,exercicesTermines:termine,challengeTermine:challenge})
+export function enregistrerResultat(id, jour, propose, termine, challenge) {
+    let a = apprenants.find(i => i.id == id)
+    if (a) {
+        if (validerResultat(jour, propose, termine, challenge)) {
+            a.resultats.push({ jour: jour, totalExercices: propose, exercicesTermines: termine, challengeTermine: challenge })
             return a
         }
     }
 }
-console.log(enregistrerResultat(3,4,20,15,true))
+console.log(enregistrerResultat(3, 4, 20, 15, true))
 
-export function rechercherApprenant(profil){
+export function rechercherApprenant(profil) {
     let a = []
-    if(!isNaN(profil)){
-        a = apprenants.filter(i => i.id == profil)
+    if (!isNaN(profil)) {
+        let p = Number(profil)
+        a = apprenants.filter(i => i.id == p)
         return a
-    }else{
+    } else {
         let nom = profil.toLowerCase().trim();
         a = apprenants.filter(i => i.nomComplet.toLowerCase().trim().includes(nom))
         return a
@@ -69,24 +71,62 @@ export function rechercherApprenant(profil){
 }
 console.log(rechercherApprenant(2))
 
-export function calculerProgression(id){
+export function calculerProgression(id) {
     let a = rechercherApprenant(id)[0];
-    let exercicesT = a.resultats.reduce((a,b)=>a+b.exercicesTermines,0)
-    let exercicesP = a.resultats.reduce((a,b)=>a+b.totalExercices,0)
-    let progression = (exercicesT/exercicesP)*100
+    let exercicesT = a.resultats.reduce((a, b) => a + b.exercicesTermines, 0)
+    let exercicesP = a.resultats.reduce((a, b) => a + b.totalExercices, 0)
+    let progression = (exercicesT / exercicesP) * 100
     let challengeT = 0;
     a.resultats.forEach(element => {
-        if(element.challengeTermine == true){
+        if (element.challengeTermine == true) {
             challengeT++
         }
     });
-    let JourneesR = a.resultats.length
+    let journeesR = a.resultats.length
+    let niveau = '';
+    if (progression >= 80 && progression <= 100) {
+        niveau = 'Solide';
+    } else if (progression >= 50 && progression <= 79) {
+        niveau = 'En progression'
+    } else if (progression < 50 && progression >= 0) {
+        niveau = 'A renforcer'
+    } else {
+        niveau = 'Non reconnu'
+    }
 
-    return (`La progression est : ${progression}, et il a terminer ${challengeT} challenges, ${JourneesR} journees enregisterer.`)
+    return {
+        exercicesT: exercicesT,
+        exercicesP: exercicesP,
+        progression: progression,
+        niveau: niveau,
+        challengeT: challengeT,
+        journeesR: journeesR
+    }
 }
 
 console.log(calculerProgression(2))
 
-export function filtrerParNiveau(){
-    
+export function filtrerParNiveau(niveau) {
+    let a = apprenants.filter(i => {
+        let cal = calculerProgression(i.id)
+        if (cal.niveau.toLowerCase() == niveau.toLowerCase()) {
+            return i
+        }
+    })
+
+    return a
 }
+
+console.log(filtrerParNiveau('solide'))
+
+
+export function trierParProgression(){
+    let a = apprenants.sort( (a,b) => {
+        let x = calculerProgression(a.id)
+        let y = calculerProgression(b.id)
+        return x.progression - y.progression
+    })
+    return a
+}
+console.log(trierParProgression())
+console.log(apprenants);
